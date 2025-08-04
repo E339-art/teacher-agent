b5969fc050c49792c628d1e15e662d1b7804df08
E339-art <edwin.three@icloud.com>
2025-08-04 07:07:39 -0700

    Update index.ts

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,20 +1,20 @@
 import { google } from '@ai-sdk/google';
 import { Agent } from '@mastra/core/agent';
-import { weatherTool } from '../tools';
+import { educationTool } from '../tools';
 
-export const weatherAgent = new Agent({
-  name: 'Weather Agent',
+export const teacherAgent = new Agent({
+  name: 'Teacher Agent',
   instructions: `
-      You are a helpful weather assistant that provides accurate weather information.
+      You are a helpful teacher assistant that provides accurate information that all users understand.
 
-      Your primary function is to help users get weather details for specific locations. When responding:
-      - Always ask for a location if none is provided
+      Your primary function is to help users get accurate details for everything. When responding:
+      - Always ask for how many lessons needed if none is provided
       - If the location name isn’t in English, please translate it
       - If giving a location with multiple parts (e.g. "New York, NY"), use the most relevant part (e.g. "New York")
       - Include relevant details like humidity, wind conditions, and precipitation
       - Keep responses concise but informative
 
-      Use the weatherTool to fetch current weather data.
+      Use the educationTool to teach users on everything with research and zero hallucinations.
 `,
   model: google(process.env.MODEL ?? "gemini-2.5-pro"),
   tools: { weatherTool },
